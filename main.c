630452f4300ff37e4782ef8a30aa16b53ad9daf7
VedantParte23 <hugstonalbert@gmail.com>
2026-03-31 23:06:57 +0530

    Setup-gihub repo

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int option;
+
+    printf("Enter your option: ");
+    scanf("%d", &option);
+
+    while (option != 5)
+    {
+        
+    }
+    
+
+
+    return 0;
+}
